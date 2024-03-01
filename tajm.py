5689fa2ba8420d4ddbf14852008aafecada9b184
David Svanberg <david.svanberg@gmail.com>
2024-03-01 16:10:51 +0100

    Some more input logic amd support for up and down arrow keys. An embryo for the TimeSlot class has been created.

--- tajm.py
+++ tajm.py
@@ -1,20 +1,127 @@
-import logging
-import re
+import logging, re, calendar
 from datetime import datetime
-from textual import on
+from textual import on, events
 from textual.app import App, ComposeResult
-from textual.containers import Container, Horizontal
+from textual.containers import Container, Horizontal, Vertical
 from textual.validation import Function, Number, ValidationResult, Validator
 from textual.widgets import Header, Footer, Label, Input, Static, Tabs, TextArea, Button
+from time_slot import TimeSlot
 
-class ValidTime(Validator):  
+class ValidYear(Validator):  
     def validate(self, value: str) -> ValidationResult:
-        """Check a string is equal to its reverse."""
-        pattern = re.compile("[0-2][0-9]:[0-5][0-9]")
-        if pattern.fullmatch(value) != None:
+        if value.isdigit() and int(value) >= 1 and int(value) <= 9999: 
             return self.success()
         else:
-            return self.failure("That's not a valid hh:mm")
+            return self.failure("That's not a valid year")
+
+class ValidMonth(Validator):  
+    def validate(self, value: str) -> ValidationResult:
+        if value.isdigit() and int(value) >= 1 and int(value) <= 12: 
+            return self.success()
+        else:
+            return self.failure("That's not a valid month")
+
+class ValidDay(Validator):
+
+    def __init__(self, app):
+        self.app = app
+
+    def validate(self, value: str) -> ValidationResult:
+        max_day = calendar.monthrange(self.app.selected_date.year, self.app.selected_date.month)[1]
+        if value.isdigit() and int(value) >= 1 and int(value) <= max_day: 
+            return self.success()
+        else:
+            return self.failure("That's not a valid day within the month")
+
+class ValidHour(Validator):  
+    def validate(self, value: str) -> ValidationResult:
+        if value.isdigit() and int(value) >= 0 and int(value) <= 23: 
+            return self.success()
+        else:
+            return self.failure("That's not a valid hour")
+
+class ValidMinute(Validator):  
+    def validate(self, value: str) -> ValidationResult:
+        if value.isdigit() and int(value) >= 0 and int(value) <= 59: 
+            return self.success()
+        else:
+            return self.failure("That's not a valid minute")
+
+class YearInput(Input):
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "up":
+            if ValidYear().validate(str(int(self.value) + 1)).is_valid:
+                self.value = str(int(self.value) + 1)
+        elif event.key == "down":
+            if ValidYear().validate(str(int(self.value) - 1)).is_valid:
+                self.value = str(int(self.value) - 1)
+
+    def on_blur(self):
+        if ValidYear().validate(self.value).is_valid:
+            try:
+                new_date = self.app.selected_date.replace(year=int(self.value))
+                self.app.update_selected_date(new_date)
+            except:
+                self.app.notify("Invalid date!", severity="error") 
+
+class MonthInput(Input):
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "up":
+            if ValidMonth().validate(str(int(self.value) + 1)).is_valid:
+                self.value = str(int(self.value) + 1).zfill(2)
+        elif event.key == "down":
+            if ValidMonth().validate(str(int(self.value) - 1)).is_valid:
+                self.value = str(int(self.value) - 1).zfill(2)
+
+    def on_blur(self):
+        if ValidMonth().validate(self.value).is_valid:
+            try:
+                new_date = self.app.selected_date.replace(month=int(self.value))
+                self.app.update_selected_date(new_date)
+            except:
+                self.app.notify("Invalid date!", severity="error") 
+
+class DayInput(Input):
+    def on_key(self, event: events.Key) -> None:
+        max_day = calendar.monthrange(self.app.selected_date.year, self.app.selected_date.month)[1]
+        if event.key == "up":
+            if ValidDay(self.app).validate(str(int(self.value) + 1)).is_valid:
+                self.value = str(int(self.value) + 1).zfill(2)
+        elif event.key == "down":
+            if ValidDay(self.app).validate(str(int(self.value) - 1)).is_valid:
+                self.value = str(int(self.value) - 1).zfill(2)
+
+    def on_blur(self):
+        if ValidDay(self.app).validate(self.value).is_valid:
+            try:
+                new_date = self.app.selected_date.replace(day=int(self.value))
+                self.app.update_selected_date(new_date)
+            except:
+                self.app.notify("Invalid date!", severity="error") 
+
+class HourInput(Input):
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "up":
+            if ValidHour().validate(str(int(self.value) + 1)).is_valid:
+                self.value = str(int(self.value) + 1).zfill(2)
+        elif event.key == "down":
+            if ValidHour().validate(str(int(self.value) - 1)).is_valid:
+                self.value = str(int(self.value) - 1).zfill(2)
+
+    def on_blur(self):
+        self.app.update_slot_summary()
+
+class MinuteInput(Input):
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "up":
+            if ValidMinute().validate(str(int(self.value) + 1)).is_valid:
+                self.value = str(int(self.value) + 1).zfill(2)
+        elif event.key == "down":
+            if ValidMinute().validate(str(int(self.value) - 1)).is_valid:
+                self.value = str(int(self.value) - 1).zfill(2)
+
+    def on_blur(self):
+        self.app.update_slot_summary()
 
 class Tajm(App):
     """ A Textual app to manage time slots """
@@ -23,9 +130,12 @@ class Tajm(App):
     CSS_PATH = "layout.tcss"
     AUTO_FOCUS = "#end_time"
 
+    selected_date = datetime.now()
+
+    time_slot = None
+
     def compose(self) -> ComposeResult:
-        now = datetime.now()
-        logging.debug(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
+        logging.debug(f"{self.selected_date.strftime("%Y-%m-%d %H:%M:%S")}")
 
         """Create child widgets for the app."""
         yield Header()
@@ -33,43 +143,79 @@ class Tajm(App):
         with Container(id="app-grid"):
             with Static("One", classes="box", id="date_row"):
                 with Horizontal():
-                    yield Input(id="year", value=f"{now.strftime("%Y")}", max_length=4)
-                    yield Input(id="month", value=f"{now.strftime("%m")}", max_length=2)
-                    yield Input(id="day", value=f"{now.strftime("%d")}", max_length=2)
-                    yield Label("Week 9", id="week")
+                    yield YearInput(id="year", value=f"{self.selected_date.strftime("%Y")}", max_length=4, validators=[ValidYear()], validate_on=["changed"])
+                    yield MonthInput(id="month", value=f"{self.selected_date.strftime("%m")}", max_length=2, validators=[ValidMonth()], validate_on=["changed"])
+                    yield DayInput(id="day", value=f"{self.selected_date.strftime("%d")}", max_length=2, validators=[ValidDay(self)], validate_on=["changed"])
+                    yield Label(id="week")
 
             with Static("Two", classes="box"):
                 with Horizontal():
-                    yield Input(id="t1h", value=f"{now.strftime("%H")}", max_length=2)
-                    yield Input(id="t1m", value=f"{now.strftime("%M")}", max_length=2)
-                    yield Input(id="t2h", value=f"{now.strftime("%H")}", max_length=2)
-                    yield Input(id="t2m", value=f"{now.strftime("%M")}", max_length=2)
-                with Horizontal():    
-                    yield Input(id="label", placeholder="Tag...", max_length=25)
-                with Horizontal():    
+                    yield HourInput(id="t1h", value=f"{self.selected_date.strftime("%H")}", max_length=2, validators=[ValidHour()], validate_on=["changed"])
+                    yield MinuteInput(id="t1m", value=f"{self.selected_date.strftime("%M")}", max_length=2, validators=[ValidMinute()], validate_on=["changed"])
+                    yield Label("to")
+                    yield HourInput(id="t2h", value=f"{self.selected_date.strftime("%H")}", max_length=2, validators=[ValidHour()], validate_on=["changed"])
+                    yield MinuteInput(id="t2m", value=f"{self.selected_date.strftime("%M")}", max_length=2, validators=[ValidMinute()], validate_on=["changed"])
+                    yield Label(id="slot_summary")
+                with Vertical():    
+                    yield Input(id="new_tag", placeholder="Tag...", max_length=25)
                     yield TextArea(id="notes")
                 with Horizontal():    
                     yield Button.success("Register")
-                    yield Button.error("Remove")
-
+                    yield Button.error("Remove", disabled=True)
             with Static("Three", classes="box"):
                 yield Tabs("Day", "Week", "Month", "Year")
-
-#                yield Input(id="start_time", value=f"{now.strftime("%H:%M")}", max_length=5, validators=[ValidTime()], validate_on=["submitted"])
-#                yield Input(id="end_time", placeholder="00:00", max_length=5, validators=[ValidTime()], validate_on=["submitted"])
+                yield Label(id="sselected")
 
         yield Footer()
+
+    def update_selected_date(self, new_date):
+        self.selected_date = new_date
+        week_label = self.query_one("#week")
+        week_label.update(f"{self.selected_date.strftime('%Y-%m-%d')} Week# {self.selected_date.isocalendar().week}")
+
+    def update_slot_summary(self):
+        """ start time """
+        t1 = self.selected_date.replace(hour=int(self.app.query_one("#t1h").value), minute=int(self.app.query_one("#t1m").value))
+        """ end time """
+        t2 = self.selected_date.replace(hour=int(self.app.query_one("#t2h").value), minute=int(self.app.query_one("#t2m").value))
+        self.time_slot = TimeSlot(t1, t2)
+        slot_summary_label = self.query_one("#slot_summary")
+        slot_summary_label.update(f"{str(self.time_slot.get_difference()[0]).zfill(1)}h {str(self.time_slot.get_difference()[1]).zfill(2)}m")
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
+    def on_mount(self):
+        self.update_selected_date(datetime.now())
+        self.update_slot_summary()
+
+    def on_tabs_tab_activated(self, event: Tabs.TabActivated) -> None:
+        """Handle TabActivated message sent by Tabs."""
+        label = self.query_one("#sselected")
+        if event.tab is None:
+            # When the tabs are cleared, event.tab will be None
+            label.visible = False
+        else:
+            label.visible = True
+            label.update(self.selected_date.strftime("%Y-%m-%d"))
+
+    def action_clear(self) -> None:
+        """Clear the tabs."""
+        self.query_one("#sselected").clear()
+
+    @on(Input.Changed)
+    def show_invalid_reasons(self, event: Input.Changed) -> None:
+        if event.validation_result != None and not event.validation_result.is_valid:
+            self.notify(event.validation_result.failure_descriptions[0], severity="error")  
 
     @on(Input.Submitted)
     def show_invalid_reasons(self, event: Input.Submitted) -> None:
-        # Updating the UI to show the reasons why validation failed
-        if not event.validation_result.is_valid:
-            self.notify(event.validation_result.failure_descriptions[0], severity="error", timeout=10)  
+        if event.input.id == "new_tag":
+            self.time_slot.add_tag(event.value)
+            # TODO ok, we have tags - but it's time to show them to the end user, also consider how to remove tags
+            """ then clear the event.input """
+            event.input.clear()
 
 if __name__ == "__main__":
     logging.basicConfig(filename='tajm.log', encoding='utf-8', level=logging.DEBUG)
---
+++ time_slot.py
@@ -0,0 +1,17 @@
+class TimeSlot:
+
+    def __init__(self, start_at, end_at):
+        self.id = None
+        self.start_at = start_at
+        self.end_at = end_at
+        self.tags = []
+        self.note = None
+        self.created_at = None
+
+    def get_difference(self):
+        """ returns the time difference between start and end as a tuple (hours, minutes)"""
+        diff = self.end_at - self.start_at
+        return (diff.seconds//3600, (diff.seconds//60)%60)
+    
+    def add_tag(self, new_tag):
+        self.tags.append(new_tag)
